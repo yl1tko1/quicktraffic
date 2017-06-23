59e4aa17eeb1107e84eebfa96c200836af4734f1
Vlad Kalnaus <vladikalanus@gmail.com>
2017-06-23 18:25:16 +0300

    Main Page QuickTraffic

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,35 @@
+var form = document.querySelector(".main-contacts-form");
+var	formSubmit = form.querySelector("button[type=submit]");
+var	inputName =  form.querySelector("[name=name]");
+var	inputEmail =  form.querySelector("[name=email]");
+var	inputPhone =  form.querySelector("[name=phone]");
+var storageName = localStorage.getItem("inputName");
+var storageEmail = localStorage.getItem("inputEmail");
+var storagePhone = localStorage.getItem("inputPhone");
+
+window.onload = function(){
+	if (storageName || storagePhone || storageEmail) {
+		inputName.value = storageName;
+		inputEmail.value = storageEmail;
+		inputPhone.value = storagePhone;
+	} else {
+		inputName.focus();
+	}
+}
+
+form.addEventListener("submit", function(e){
+	if (!inputName.value || !inputEmail.value || !inputPhone.value) {
+		e.preventDefault();
+		console.log('vvedite dannie');
+	} else {
+		e.preventDefault();
+		localStorage.setItem('inputName', inputName.value);
+		localStorage.setItem('inputEmail', inputEmail.value);
+		localStorage.setItem('inputPhone', inputPhone.value);
+		console.log('vse ok');
+	}
+});
+
 $(document).ready(function(){
 
 
